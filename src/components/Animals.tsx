d958386628a4bd788beed13e072f5bc1af1183e3
NatasaIc <iconomou.ng@gmail.com>
2023-03-08 17:15:41 +0100

    Created the animals component

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -1,55 +1,28 @@
 import { Card, Button } from 'react-bootstrap';
-import { useNavigate } from  'react-router-dom';
-import { IAnimal } from '../models/IAnimal';
+import { useState, useEffect } from  'react';
+import { IAnimalSmall } from '../models/IAnimalSmall';
+import { getAnimals } from "../services/animalServices";
 
+export function Animals(){
+    const [animals, setAnimals] = useState<IAnimalSmall[]>([]);
 
-export interface AnimalListProps extends IAnimal {
-   animal: IAnimal;
-}
+    useEffect(() => {
+        const getData = async () => {
+            let animals = await getAnimals();
 
-export function AnimalList(props: AnimalListProps){
-    const navigate = useNavigate();
+            setAnimals(animals);
+        };
+        if(animals.length > 0) return;
 
-    const showMoreInfo = () => {
-        navigate(`/animalDetails${props.animal.id}`) 
-    }
-
-    const feedMe = () => {
-
-    }
-
+        getData();
+    });
     
-
-return (
-    <Card className='h-100'>
-        <Card.Img 
-        variant='top' 
-        src={props.animal.imageUrl} height='200px'  
-        style={{ objectFit: 'cover'}}
-        />
-        <Card.Body className='d-flex flex-column'>
-
-            <Card.Title className='d-flex 
-            justify-content-between align-items-baseline
-            mb-4'>
-                <span>{props.animal.name}</span>
-                <Button variant='outline-info'
-                onClick={showMoreInfo} 
-                className='w-30'>
-                Läs mer
-                </Button>
-            </Card.Title>
-            <div className='mt-auto'>
-                <span>{props.animal.shortDescription}</span>
-            </div>
-                <Button 
-                variant='outline-info'
-                onClick={feedMe} 
-                className='w-30'>
-                {props.animal.lastFed}
-                Mata mig
-                </Button>
-        </Card.Body>
-    </Card>
-)
+let animalHtml = animals.map((animal) => {
+    return (
+        <Animal
+        
+        >
+        </Animal>
+    )
+})
 }
